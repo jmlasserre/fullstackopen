39976ab34897ab4a4a4a0fd12b75a47a5e66b223
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-02-12 23:09:54 -0300

    Exercise 1.7: unicafe step 2

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -5,17 +5,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0);
+
+  const handleGood = () => {
+    setGood(good+1);
+    setTotal(total+1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral+1);
+    setTotal(total+1);
+  }
+
+  const handleBad = () => {
+    setBad(bad+1);
+    setTotal(total+1);
+  }
 
   return (
     <div>
       <Header text="give feedback"/>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <Header text="statistics"/>
       <Display counter={good} name="good"/>
       <Display counter={neutral} name="neutral"/>
       <Display counter={bad} name="bad"/>
+      <Display counter={total} name="total"/>
+      <Display counter={(good - bad)/total} name="average"/>
+      <Display counter={(good/total)*100 + "%"} name="positive"/>
     </div>
   )
 }
