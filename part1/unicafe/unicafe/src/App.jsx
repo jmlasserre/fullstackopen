26c22071f74637fde027dda2b1fd84d5e829f9d9
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-02-13 00:28:14 -0300

    Minor correction to Exercise 1.9

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,61 +1,72 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
 
   const handleGood = () => {
-    setGood(good+1);
-    setTotal(total+1);
-  }
+    setGood(good + 1);
+    setTotal(total + 1);
+  };
 
   const handleNeutral = () => {
-    setNeutral(neutral+1);
-    setTotal(total+1);
-  }
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  };
 
   const handleBad = () => {
-    setBad(bad+1);
-    setTotal(total+1);
-  }
-  
+    setBad(bad + 1);
+    setTotal(total + 1);
+  };
+
   return (
     <div>
-      <Header text="give feedback"/>
+      <Header text="give feedback" />
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <Header text="statistics"/>
-      <Statistics total={total} stats={[{value: good, name: "good"},
-                        {value: neutral, name: "neutral"},
-                        {value: bad, name: "bad"},
-                        {value: total, name: "total"},
-                        {value: (good - bad)/total, name: "average"},
-                        {value: (good/total)*100 + "%", name: "positive"},
-                        ]}/>
+      <Header text="statistics" />
+      <Statistics
+        total={total}
+        stats={[
+          { value: good, name: "good" },
+          { value: neutral, name: "neutral" },
+          { value: bad, name: "bad" },
+          { value: total, name: "total" },
+          { value: (good - bad) / total, name: "average" },
+          { value: (good / total) * 100 + "%", name: "positive" },
+        ]}
+      />
     </div>
-  )
-}
+  );
+};
 
-const Header = ({ text }) => <h1>{text}</h1>
+const Header = ({ text }) => <h1>{text}</h1>;
 
-const Display = ({counter, name}) => <p>{name} {counter}</p>
+const Display = ({ counter, name }) => (
+  <p>
+    {name} {counter}
+  </p>
+);
 
 const Statistics = ({ total, stats }) => {
-  if (!total) return (
-    <div>
-      <p>No feedback given</p>
-    </div>
-  )
-  let map = stats.map((e) => <p key={e.name}>{e.name} {e.value}</p>);
-  return (
-    <div>
-      {map}
-    </div>
-  )
-}
+  if (!total)
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  else {
+    let map = stats.map((e) => (
+      <p key={e.name}>
+        {e.name} {e.value}
+      </p>
+    ));
+    return <div>{map}</div>;
+  }
+};
 
-export default App
+export default App;
