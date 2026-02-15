c1741b479ad5e5e3ca323054e027a3fd2f4532d2
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-02-15 15:33:19 -0300

    Exercise 1.13: anecdotes step 2

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,30 +1,79 @@
 import { useState } from "react";
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const [selected, setSelected] = useState(0);
+  const handleGood = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  };
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>
-        Next anecdote
-      </button>
+      <Header text="give feedback" />
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+      <Header text="statistics" />
+      <Statistics total={total} good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
 
-// Based on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Display = ({ counter, name }) => (
+  <p>
+    {name} {counter}
+  </p>
+);
+
+const Statistics = ({ total, good, neutral, bad }) => {
+  if (!total)
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={((good - bad) / total).toFixed(1)} />
+            <StatisticLine text="positive" value={((good / total) * 100).toFixed(1) + " %"} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
 export default App;
