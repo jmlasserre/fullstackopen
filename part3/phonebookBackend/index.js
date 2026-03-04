391e0805f0da1b67b113a2c3d06cb271fab9a62e
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-03-04 14:56:50 -0300

    Slight changes for 3.9

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 morgan.token(`body`, (req, res) => JSON.stringify(req.body))
@@ -28,6 +29,8 @@ let notes = [
 ]
 
 app.use(express.json());
+app.use(cors())
+app.use(express.static('dist'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (req, res) => {
@@ -76,7 +79,7 @@ app.post('/api/persons', (req, res) => {
     res.json(notes);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
