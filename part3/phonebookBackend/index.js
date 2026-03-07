522417f11a59ecbcc9e87dcf8f8758282bf33672
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-03-07 14:53:03 -0300

    3.15: Phonebook database, step 3

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,19 +1,15 @@
-// I did 3.14 while doing 3.13. My bad.
-
 require("dotenv").config();
 const dns = require("node:dns/promises");
 dns.setServers(["1.1.1.1"]); // Found this fix online: https://www.mongodb.com/community/forums/t/error-querysrv-econnrefused-mongodb/259042/4
 const Person = require("./models/person");
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 
 morgan.token(`body`, (req, res) => JSON.stringify(req.body));
 
-app.use(express.json());
-app.use(cors());
 app.use(express.static("dist"));
+app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
@@ -32,33 +28,40 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
-  .then((person) => {
-    res.json(person);
-  })
-  .catch(() => {
-    res.status(404).end();
-  });
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  names = names.filter((newName) => newName.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (req, res) => {
-  const nameToReplace = req.body.name;
-  const newNumber = req.body.number;
-  const existingName = names.find(
-    (otherName) => otherName.name === nameToReplace,
-  );
-  const newName = { ...existingName, number: newNumber };
-  names = names.map((otherName) =>
-    otherName.name === nameToReplace ? newName : otherName,
-  );
-  return res.json(req.body);
+app.put("/api/persons/:id", (req, res, next) => {
+  const number = req.body;
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        person.number = number;
+        person.save().then((savedPerson) => {
+          res.json(savedPerson);
+        });
+      } else {
+        return res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -80,7 +83,21 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-// Finally fixed it. The path was incorrectly typed, meaning it never copied 'dist' into the root directory. My bad.
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
