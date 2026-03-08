d6f1cd008d2cd55e3ad2dfa0de1910083e81bf8c
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-03-08 01:21:02 -0300

    3.20: Phonebook database, step 8

--- part3/phonebookBackend/models/person.js
+++ part3/phonebookBackend/models/person.js
@@ -22,7 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true,
+    minLength: 8,
+    required: [true, 'A phone number is required'],
+    validate: {
+      validator: function(v){
+        return /^\d{2,3}-\d{6,}$/.test(v);
+      },
+      message: props => `${props.value} is an invalid phone number.`
+    }
   },
 });
 
