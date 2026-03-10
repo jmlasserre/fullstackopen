0522f65a27149eaf42eedcfd5dd5570ee48396a5
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-03-10 20:26:03 -0300

    4.5: Helper Functions and Unit Tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,4 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, curr) => sum + curr.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  let favoriteBlogIndex = 0
+  let mostLikes = 0
+  blogs.forEach(b => {
+    if (b.likes > mostLikes){
+      favoriteBlogIndex = blogs.indexOf(b)
+      mostLikes = b.likes
+    }
+  })
+  return blogs[favoriteBlogIndex]
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
