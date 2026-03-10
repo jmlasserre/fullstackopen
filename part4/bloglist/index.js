4c0f7a0485433e48d72c76c2053a196453a65ed9
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2026-03-10 16:21:31 -0300

    4.1: Blog List, step 1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const dns = require("node:dns/promises");
+dns.setServers(["1.1.1.1"]);
+
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+mongoose.connect(MONGODB_URI, { family: 4 });
+
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
